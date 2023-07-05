b50fe307b8a3e2668812bc23f49ad05a2e51e2ff
Manvel Parsadanov <mparsadanov@gmail.com>
2023-07-05 16:10:51 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,26 +2,32 @@
 
 def resize_from_center(bbox, scale_w, scale_h, scale_both):
 
-	if scale_both:
-		scale_h = scale_w
+    if scale_both:
+        scale_h = scale_w
 
-	if scale_w == 0 or scale_h == 0:
-		return bbox
+    if scale_w == 0 or scale_h == 0:
+        return bbox 
 
+    kw = bbox[2] * scale_w - bbox[2]
+    kh = bbox[3] * scale_h - bbox[3]
+    
+    bbox[0] -= kw / 2
+    bbox[1] -= kh / 2
+    bbox[2] += kw
+    bbox[3] += kh
+ 
+    if bbox[0] < 0:
+        bbox[2] += bbox[0]
+        bbox[0] = 0
 
-	wim = bbox[2]
-	him = bbox[3]
+    if bbox[1] < 0:
+        bbox[3] += bbox[1]
+        bbox[1] = 0
 
-	dx = scale_w * wim
-	dy = scale_h * him
+    if bbox[0] + bbox[2] > 1:
+        bbox[2] = 1 - bbox[0]
 
-	xc = bbox[0] + wim/2
-	yc = bbox[1] + him/2
+    if bbox[1] + bbox[3] > 1:
+        bbox[3] = 1 - bbox[1]
 
-
-	x = round(max(xc-dx/2, 0), 3)
-	y = round(max(yc-dy/2, 0), 3)
-	w = round(min(dx, 1), 3)
-	h = round(min(dy, 1), 3)
-
-	return [x,y,w,h]
+    return bbox
